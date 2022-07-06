21715f7d130b8234496cd814e19afbe4fc7e19bc
lucia gonzalez <luciabegeve@gmail.com>
2022-07-06 03:35:02 -0300

    termine codigo de ejercicio7, condicionales de 3 numeros

--- js/ejercicios/tp1-ej7.js
+++ js/ejercicios/tp1-ej7.js
@@ -1,13 +1,25 @@
 //Escribe un programa que pida 3 números y escriba en la pantalla el mayor de los tres.
-let a = 4
-let b = 7
-let c = 10;
+let numero1 = parseInt(prompt("Ingrese un primer numero"));
+let numero2 = parseInt(prompt("Ingrese un segundo numero"));
+let numero3 = parseInt(prompt("Ingrese un tercer numero"));
+console.log(numero1);
+console.log(numero2);
+console.log(numero3);
 
-if(a>b){
-    console.log ('a es mayor que b');
-} else if (a>c){
-    console.log('si a es mayor que c y b');
+if (numero1 > numero2) {
+  console.log("n1 es mayor que n2");
+  document.write("el primer numero es el mayor, numero ingresado " + numero1);
+} else if (numero2 > numero3) {
+    console.log('El n2 es mayor al n3')
+  document.write(
+    "el segundo numero es mayor que el primer numero, numero ingresado" + numero2);
+} else if (numero3 > numero1){
+    console.log('el n3 es mayor a n1 y n2')
+    document.write('el numero 3 es mayor al numero 1 y 2, numero ingresado '+ numero3);
 }
+
+
+/*
 if(b>a){
     console.log('b es mayor que a');
 } else if (b>c){
@@ -17,4 +29,4 @@ if(c>a){
     console.log('c es mayor que a');
 } else if (c>b){
     console.log('si c es mayor que b y a ');
-}
+}*/
