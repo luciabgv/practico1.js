4bfba566ffc893e8c4432c82970b0609498b9504
lucia gonzalez <luciabegeve@gmail.com>
2022-07-06 02:39:51 -0300

    termine ejercicio4

--- js/ejercicios/tp1-ej4.js
+++ js/ejercicios/tp1-ej4.js
@@ -1,5 +1,5 @@
 //Escribe un programa de dos líneas que pida el nombre del usuario con un prompt y escriba un texto que diga «Hola nombreUsuario»
 
-let nombreUsuario = 'luciabgv'
+let nombreUsuario = 'luciagv'
 
 nombreUsuario = (prompt( ' Hola ' + nombreUsuario));
