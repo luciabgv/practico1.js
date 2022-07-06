8cbe27f72c68fc889c5cc948a2d7ca3ede1355c1
lucia gonzalez <luciabegeve@gmail.com>
2022-07-05 23:56:48 -0300

    primeros ejercicios de javaScript 1er practico

---
+++ js/practica.js
@@ -0,0 +1,10 @@
+//alert'un mensaje'
+alert('un mensaje');
+//document.write 'hello world'
+document.write('<h1> Hello World </h1>');
+// Resultado de sumar 3+5
+document.write( 3 + 5)
+//Escribe un programa de dos líneas que pida el nombre del usuario con un prompt y escriba un texto que diga «Hola nombreUsuario»
+
+
+document.write(prompt())
