d9d3f877b6bfac8d51e5d12e79758400ab2f0be4
lucia gonzalez <luciabegeve@gmail.com>
2022-07-06 02:18:08 -0300

    termine ejercicio 5 de sumatoria

--- js/ejercicios/tp1-ej5.js
+++ js/ejercicios/tp1-ej5.js
@@ -1,7 +1,8 @@
 // Escribe un programa de tres líneas que pida un número, pida otro número y escriba el resultado de sumar estos dos números.
 
-let numero1 = parseInt(prompt('Ingrese un primer numero');)
-let numero2 = prompt('Ingrese un primer numero');
-console.log(parseInt(numero1))
-console.log(parseInt(numero2))
+let numero1 = parseInt(prompt('Ingrese un primer numero'));
+let numero2 = parseInt(prompt('Ingrese un segundo numero'));
+console.log(numero1)
+console.log(numero2)
+
 document.write( '<p>Resultado: ' + ( numero1 + numero2 ) + '</p>' );
